d95f3eaacd3eac4b73ea06492f0aebf71f421725
unknown <santiago_alarconal@fet.edu.co>
2026-09-14 23:41:39 -0500

    Elimina node_modules y .angular del repositorio

--- .angular/cache/22.0.1/app/vite/deps/ionicons.js
+++
@@ -1,9 +0,0 @@
-import {
-  addIcons,
-  setAssetPath
-} from "./chunk-GLIKV77K.js";
-import "./chunk-PAXKX5KU.js";
-export {
-  addIcons,
-  setAssetPath
-};
--- .angular/cache/22.0.1/app/vite/deps/p-D0YpjgON-STYBKLQC.js
+++
@@ -1,9 +0,0 @@
-import {
-  h,
-  n
-} from "./chunk-YEAQTUYE.js";
-import "./chunk-PAXKX5KU.js";
-export {
-  h as GESTURE_CONTROLLER,
-  n as createGesture
-};
